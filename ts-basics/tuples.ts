01062c00eb00d1876a8a7563e329791c2b0781d4
vllau <vincent.lau03@gmail.com>
2022-09-25 01:58:01 +1000

    Lesson 6

---
+++ ts-basics/tuples.ts
@@ -0,0 +1,31 @@
+type ThreeDCoordinate = [x: number, y: number, z: number];
+
+function add3DCoordinates(
+  c1: ThreeDCoordinate,
+  c2: ThreeDCoordinate
+): ThreeDCoordinate {
+  return [c1[0] + c2[0], c1[1] + c2[1], c1[2] + c2[2]];
+}
+
+console.log(add3DCoordinates([0, 0, 0], [10, 20, 30]));
+
+function simpleStringState(
+  initial: string
+): [() => string, (v: string) => void] {
+  let str: string = initial;
+
+  return [
+    () => str,
+    (v: string) => {
+      str = v;
+    },
+  ];
+}
+
+const [str1getter, str1setter] = simpleStringState("hello");
+const [str2getter, str2setter] = simpleStringState("vincent");
+console.log(str2getter());
+console.log(str1getter());
+str1setter("goodbye");
+console.log(str1getter());
+console.log(str2getter());
